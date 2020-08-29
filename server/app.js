0438bea12a3962fe3bd53e53205a3601238e56a5
katz <esty3149@gmail.com>
2020-08-30 01:07:38 +0300

    connect to sql

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const express = require('express');
 const routes = require('./routes/userRoutes');
 const bodyParser = require('body-parser');
+
 // require('dotenv').config()
 
 
@@ -12,5 +13,39 @@ app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
 
 app.use('/user', routes);
-
+app.get('/',(req,res)=>{
+    const sql=require('mssql');
+    var config={
+        user:'WINDOWS-J21BCB7\Admin',
+        password:'',
+        server:'localhost',
+        database:'ShovalSwimmingCourses'
+    };
+    var conn = new sql.Connection(config);
+    conn.connect(config, function (err) {
+         if (err) console.log(err);
+         var request = new sql.Request(conn);
+         request.query('select * from Student', function (err, recordset) {
+        if(err) console.log(err);
+         res.send(recordset);
+        sql.close();
+         });
+     });
+   // var req = new sql.Request(conn);
+//      conn.connect(function (err) {
+//        if (err) {
+//            console.log(err);
+//            return;
+//         }
+//         req.query("SELECT * FROM Student", function (err, recordset) {
+//            if (err) {
+//                console.log(err);
+//            }
+//            else { 
+//                console.log(recordset);
+//            }
+//            conn.close();
+//        });
+//  });
+});
 console.log('todo list RESTful API server started on: ' + port);
