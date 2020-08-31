9b98842833734ead32a55e1487eeb045489133bb
katz <esty3149@gmail.com>
2020-08-31 20:40:18 +0300

    app

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ app.get('/', function (req, res) {
             if (err) console.log(err)
 
             // send records as a response
-            res.send(recordset);
+           // res.send(recordset);
         } );
     });
 
