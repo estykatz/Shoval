274f609ae0c98af5046d3272ea89c7a977e978e5
unknown <esty3149@gmail.com>
2020-09-09 18:32:05 +0300

    kjnhbgvf

---
+++ client/src/app/models/newUser.ts
@@ -0,0 +1,20 @@
+import { publicDecrypt } from "crypto"
+
+export class NewUser{
+Id:number
+password:string
+userName:string
+userAdmin:boolean
+email:string
+
+
+
+constructor (id:number,pd:string,user:string,admin:boolean,em:string)
+{
+    this.Id=id;
+    this.password=pd;
+this.userName=name;
+this.userAdmin=admin;
+this.email=em;
+}
+}
