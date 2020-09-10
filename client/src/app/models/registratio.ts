ba2841fcc695d34109c3bc63e7671ed343da46e5
unknown <esty3149@gmail.com>
2020-09-10 17:53:56 +0300

    frtgyhu

---
+++ client/src/app/models/registratio.ts
@@ -0,0 +1,27 @@
+export class registratio{
+    identity:number
+     stLastName:string
+stFirstName:string
+phone:string
+PlaceOfStudy:string
+SwimmingLevels:number//אני לא יודעת מאיזה טיפוס כדאי לעשות אותו
+Debt:number//טיפוס number מביא מספרים מדיוקים גם אחרי הנקודה?
+Discount:boolean
+Remarks:string  
+
+
+constructor
+(id:number,stLast:string,stFirst:string,ph:string,plas:string,swimming:number
+    ,debt:number,discount:boolean,remarks:string){
+        this.identity=id;
+        this.stLastName=stLast;
+        this.stFirstName=stLast;
+        this.phone=ph;
+        this.PlaceOfStudy=plas;
+        this.SwimmingLevels=swimming;
+        this.Debt=debt;
+        this.Discount=discount;
+    this.Remarks=remarks;
+ }
+   
+}
